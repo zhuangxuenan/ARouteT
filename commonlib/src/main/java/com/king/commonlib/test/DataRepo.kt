e2daf338bf49f56eeacfd0f457296ee27e2d6650
zhuangxuenan <xuenan456@yeah.net>
2019-09-16 16:55:05 +0800

    修改

---
+++ commonlib/src/main/java/com/king/commonlib/test/DataRepo.kt
@@ -0,0 +1,21 @@
+package com.king.commonlib.test
+
+/**
+ *
+ * @ProjectName:    ARouteT$
+ * @Package:        com.king.commonlib.test$
+ * @ClassName:      DataRepo$
+ * @Description:     java类作用描述
+ * @Author:         庄学南
+ * @CreateDate:     2019/9/16$ 14:46$
+ * @UpdateUser:     更新者：
+ * @UpdateDate:     2019/9/16$ 14:46$
+ * @UpdateRemark:   更新说明：
+ */
+//文章
+data class ArticleResponseBody(
+    val name: String,
+    val url: String
+){
+
+}
